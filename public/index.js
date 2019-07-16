2611d84b27873179ebdd782d110d5fe5ff653624
KAB <47466350+KwasiBell@users.noreply.github.com>
2019-07-16 15:39:28 -0400

    added note

--- public/index.js
+++ public/index.js
@@ -1,30 +1,90 @@
-$('document').ready(function() {
+$('document').ready(function () {
 
-  $('.save_article_btn').on("click", function(e) {
+  $('.save_article_btn').on("click", function (e) {
     e.preventDefault();
     var articleTitle = $(this).data("title");
     var articleId = $(this).data("id");
-    console.log("article clicked");
-    console.log(articleId);
+    // you can use jquery to target elements and do what ever you want in this function after the data comes back from the server
+    // $.(`/articles/${articleId}`)
 
-    // the parameter e is the event location on the DOM
-
-      // you can use jquery to target elements and do what ever you want in this function after the data comes back from the server
-      $.post( `/articles/${articleId}`)
-       .done(function( data ) {
-        console.log(`Load was performed. Article clicked: ${articleTitle}`);
-        alert( "Data Loaded: " + data );
-      });
-      // $( ".result" ).html( data );
-      //  $( "." ).html( data );
-
-
-
-
+    $.ajax({
+      type: 'PUT',
+      url: `/articles/${articleId}`,
+      contentType: 'application/json',
+      data: JSON.stringify({"saved": true}), // access in body
+    }).done(function (data) {
+      console.log(`Load was performed. Article clicked: ${articleTitle}`);
+      alert("Data Loaded: " + data);
+    }).fail(function (msg) {
+      console.log('FAIL');
+    }).always(function (msg) {
+      console.log('ALWAYS');
+    });
+    // $( ".result" ).html( data );
+    //  $( "." ).html( data );
   })
 
 
+  function savedArticleRoutine() {
+
+  }
 
 
+
+  function addNoteRoutine() {
+    // add note to the article id
+    // show modal
+    const articleId = $(this).data("id");
+    $('.saved-note-modal').show();
+    $('.save-submit-btn').on("click", (e) => {
+      e.preventDefault();
+      const articleNote = $("#article-note").val();
+      const noteTitle = $("#note-title").val();
+      // note should be added to the Notes model
+      $.post( `/articles/${articleId}`, {
+        title: noteTitle,
+        body: articleNote
+      })
+      .done(function( data ) {
+        $("#article-note").empty();
+        $("#note-title").empty();
+        $('.saved-note-modal').hide();
+        alert( "Data Loaded: " + data );
+      });
+
+    })
+
+  }
+
+  function viewNotes() {
+    const articleId = $(this).data("id");
+    $('.previous-notes-modal').show();
+    $.get( `/articles/${articleId}`)
+    .done(function(data) {
+      // $('.previous-notes-modal').hide();
+      console.log(data.note);
+      let notesContent = $(`<h3>hello<h3>`);
+      // notesContent.html(JSON.stringify(data.note));
+      $('#previous-notes').append(notesContent);
+    })
+    .fail(function () {
+      alert("could not retrieve data")
+    })
+  }
+
+  function closeModal() {
+    $('.saved-note-modal').hide();
+  }
+
+  $('.add-note, .saved-article').on("click", addNoteRoutine);
+  $('.view-note').on("click", viewNotes);
+
+  $('.modal-close').on("click", () => {
+    $('.saved-note-modal').hide();
+  });
+
+  $('.previous-notes-close').on("click", () => {
+    $('.previous-notes-modal').hide();
+  });
 
 })
--- server.js
+++ server.js
@@ -167,7 +167,7 @@ app.get("/articles/:id", function(req, res) {
 });
 
 // Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
+app.put("/articles/:id", function(req, res) {
   // TODO
   // ====
   // save the new note that gets posted to the Notes collection
@@ -199,6 +199,26 @@ app.post("/articles/:id", function(req, res) {
         }
     });
   });
+
+
+  // add note to article
+  app.post("/articles/:id", function(req, res) {
+    db.Note.create(req.body)
+    .then(function(dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+  })
 
 // Start the server
 app.listen(PORT, function() {
